9cc119494533b2e510720f7cad516be7b8f0c9b6
yubing <29988076@qq.com>
2017-08-25 19:28:16 +0800

    ..

--- src/components/main/fullView.vue
+++ src/components/main/fullView.vue
@@ -119,6 +119,8 @@ export default {
     distribution() {
       let chinaDom = document.getElementsByClassName('fullView_china')[0];
       var myChart = echarts.init(chinaDom);
+      var app = {};
+     var option = null;
       var geoCoordMap = {
         '海门': [121.15, 31.89],
         '鄂尔多斯': [109.781327, 39.608266],
@@ -346,6 +348,7 @@ export default {
         { name: '黑龙江', value: 3743.29 }
       ];
 
+
       var convertData = function(data) {
         var res = [];
         for (var i = 0; i < data.length; i++) {
@@ -360,74 +363,66 @@ export default {
         return res;
       };
 
-      var convertedData = [
-        convertData(data),
-        convertData(data.sort(function(a, b) {
-          return b.value - a.value;
-        }).slice(0, 5))
-      ];
-
-     let option = {
-        series: [{
-          type: 'map',
-          map: 'china'
-        }],
+      option = {
         backgroundColor: '#404a59',
-        animation: true,
-        animationDuration: 1000,
-        animationEasing: 'cubicInOut',
-        animationDurationUpdate: 1000,
-        animationEasingUpdate: 'cubicInOut',
         title: [{
-            text: '产业全国布局',
-            subtext: '',
-            sublink: '',
-            left: 'center',
-            textStyle: {
-              color: '#fff'
-            }
-          },
-          {
-            id: 'statistic',
-            right: 120,
-            top: 40,
-            width: 100,
-            textStyle: {
-              color: '#fff',
-              fontSize: 16
-            }
+          text: '全国主要城市空气质量',
+          subtext: 'data from PM25.in',
+          sublink: 'http://www.pm25.in',
+          left: 'center',
+          textStyle: {
+            color: '#fff'
           }
-        ],
-        toolbox: {
-          iconStyle: {
-            normal: {
-              borderColor: '#fff'
-            },
-            emphasis: {
-              borderColor: '#b1e4ff'
-            }
+        }, {
+          id: 'statistic',
+          right: 120,
+          top: 40,
+          width: 100,
+          textStyle: {
+            color: '#fff',
+            fontSize: 16
+          }
+        }],
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          orient: 'vertical',
+          y: 'bottom',
+          x: 'right',
+          data: ['pm2.5'],
+          textStyle: {
+            color: '#fff'
           }
         },
-        brush: {
-          outOfBrush: {
-            color: '#abc'
-          },
-          brushStyle: {
-            borderWidth: 2,
-            color: 'rgba(0,0,0,0.2)',
-            borderColor: 'rgba(0,0,0,0.5)',
-          },
-          seriesIndex: [0, 1],
-          throttleType: 'debounce',
-          throttleDelay: 300,
-          geoIndex: 0
+        grid: {
+          right: 40,
+          top: 100,
+          bottom: 40,
+          width: '200px'
+        },
+        xAxis: {
+          type: 'value',
+          scale: true,
+          position: 'top',
+          axisLabel: { show: !!0 },
+          boundaryGap: false,
+          splitLine: { show: false },
+          axisLine: { show: false },
+          axisTick: { show: false },
+          axisLabel: { margin: 2, textStyle: { color: '#aaa' } },
+        },
+        yAxis: {
+          type: 'category',
+          name: 'TOP 20',
+          nameGap: 16,
+          axisLine: { show: false, lineStyle: { color: '#ddd' } },
+          axisTick: { show: false, lineStyle: { color: '#ddd' } },
+          axisLabel: { interval: 0, textStyle: { color: '#ddd' } },
+          data: ["房地产", "交通运输", "制造业", "能源", "电子", "现代服务", "商贸", "建材", "TMT", "旅游", "化工", "农业", "冶金", "矿业", "公用事业", "金融", "物流", "科研", "建筑工程", "石油石化"].reverse()
         },
         geo: {
           map: 'china',
-          left: '10',
-          right: '35%',
-          center: [117.98561551896913, 31.205000490896193],
-          zoom: 2.5,
           label: {
             emphasis: {
               show: false
@@ -444,39 +439,11 @@ export default {
             }
           }
         },
-        tooltip: {
-          trigger: 'item'
-        },
-        grid: {
-          right: 40,
-          top: 100,
-          bottom: 40,
-          width: '30%'
-        },
-        xAxis: {
-          type: 'value',
-          scale: true,
-          position: 'top',
-          boundaryGap: false,
-          splitLine: { show: false },
-          axisLine: { show: false },
-          axisTick: { show: false },
-          axisLabel: { margin: 2, textStyle: { color: '#aaa' } },
-        },
-        yAxis: {
-          type: 'category',
-          name: 'TOP 20',
-          nameGap: 16,
-          axisLine: { show: false, lineStyle: { color: '#ddd' } },
-          axisTick: { show: false, lineStyle: { color: '#ddd' } },
-          axisLabel: { interval: 0, textStyle: { color: '#ddd' } },
-          data: []
-        },
         series: [{
-            name: '产值',
+            name: 'pm2.5',
             type: 'scatter',
             coordinateSystem: 'geo',
-            data: convertedData[0],
+            data: convertData(data),
             symbolSize: function(val) {
               return val[2] / 1000;
             },
@@ -490,18 +457,6 @@ export default {
                 show: true
               }
             },
-            tooltip: {
-              formatter: function(param) {
-                var re = '';
-                re += param.seriesName;
-                re += '<br />';
-                re += param.name;
-                re += '：';
-                re += param.value[2];
-                re += '（亿元）';
-                return re;
-              }
-            },
             itemStyle: {
               normal: {
                 color: '#ddb926'
@@ -509,28 +464,18 @@ export default {
             }
           },
           {
-            name: 'Top 5',
+            name: 'Top5',
             type: 'effectScatter',
             coordinateSystem: 'geo',
-            data: convertedData[1],
+            data: convertData(data.sort(function(a, b) {
+              return b.value - a.value;
+            }).slice(0, 5)),
             symbolSize: function(val) {
               return val[2] / 1000;
             },
-            showEffectOn: 'emphasis',
+            showEffectOn: 'render',
             rippleEffect: {
               brushType: 'stroke'
-            },
-            tooltip: {
-              formatter: function(param) {
-                var re = '';
-                re += param.seriesName;
-                re += '<br />';
-                re += param.name;
-                re += '：';
-                re += param.value[2];
-                re += '（亿元）';
-                return re;
-              }
             },
             hoverAnimation: true,
             label: {
@@ -559,263 +504,81 @@ export default {
                 color: '#ddb926'
               }
             },
-            data: []
+            data: ["76788", "27472", "21018", "16101", "14358", "13795", "12773", "10991", "10268", "9592", "7734", "5934", "4878", "4825", "4369", "3926", "3799", "3195", "2095", "1563"].reverse()
           }
         ]
       };
-
-      myChart.on('brushselected', renderBrushed);
-
-      setTimeout(function() {
-        myChart.dispatchAction({
-          type: 'brush',
-          areas: [{
-            geoIndex: 0,
-            brushType: 'polygon',
-            coordRange: [
-              [119.72, 34.85],
-              [119.68, 34.85],
-              [119.5, 34.84],
-              [119.19, 34.77],
-              [118.76, 34.63],
-              [118.6, 34.6],
-              [118.46, 34.6],
-              [118.33, 34.57],
-              [118.05, 34.56],
-              [117.6, 34.56],
-              [117.41, 34.56],
-              [117.25, 34.56],
-              [117.11, 34.56],
-              [117.02, 34.56],
-              [117, 34.56],
-              [116.94, 34.56],
-              [116.94, 34.55],
-              [116.9, 34.5],
-              [116.88, 34.44],
-              [116.88, 34.37],
-              [116.88, 34.33],
-              [116.88, 34.24],
-              [116.92, 34.15],
-              [116.98, 34.09],
-              [117.05, 34.06],
-              [117.19, 33.96],
-              [117.29, 33.9],
-              [117.43, 33.8],
-              [117.49, 33.75],
-              [117.54, 33.68],
-              [117.6, 33.65],
-              [117.62, 33.61],
-              [117.64, 33.59],
-              [117.68, 33.58],
-              [117.7, 33.52],
-              [117.74, 33.5],
-              [117.74, 33.46],
-              [117.8, 33.44],
-              [117.82, 33.41],
-              [117.86, 33.37],
-              [117.9, 33.3],
-              [117.9, 33.28],
-              [117.9, 33.27],
-              [118.09, 32.97],
-              [118.21, 32.7],
-              [118.29, 32.56],
-              [118.31, 32.5],
-              [118.35, 32.46],
-              [118.35, 32.42],
-              [118.35, 32.36],
-              [118.35, 32.34],
-              [118.37, 32.24],
-              [118.37, 32.14],
-              [118.37, 32.09],
-              [118.44, 32.05],
-              [118.46, 32.01],
-              [118.54, 31.98],
-              [118.6, 31.93],
-              [118.68, 31.86],
-              [118.72, 31.8],
-              [118.74, 31.78],
-              [118.76, 31.74],
-              [118.78, 31.7],
-              [118.82, 31.64],
-              [118.82, 31.62],
-              [118.86, 31.58],
-              [118.86, 31.55],
-              [118.88, 31.54],
-              [118.88, 31.52],
-              [118.9, 31.51],
-              [118.91, 31.48],
-              [118.93, 31.43],
-              [118.95, 31.4],
-              [118.97, 31.39],
-              [118.97, 31.37],
-              [118.97, 31.34],
-              [118.97, 31.27],
-              [118.97, 31.21],
-              [118.97, 31.17],
-              [118.97, 31.12],
-              [118.97, 31.02],
-              [118.97, 30.93],
-              [118.97, 30.87],
-              [118.97, 30.85],
-              [118.95, 30.8],
-              [118.95, 30.77],
-              [118.95, 30.76],
-              [118.93, 30.7],
-              [118.91, 30.63],
-              [118.91, 30.61],
-              [118.91, 30.6],
-              [118.9, 30.6],
-              [118.88, 30.54],
-              [118.88, 30.51],
-              [118.86, 30.51],
-              [118.86, 30.46],
-              [118.72, 30.18],
-              [118.68, 30.1],
-              [118.66, 30.07],
-              [118.62, 29.91],
-              [118.56, 29.73],
-              [118.52, 29.63],
-              [118.48, 29.51],
-              [118.44, 29.42],
-              [118.44, 29.32],
-              [118.43, 29.19],
-              [118.43, 29.14],
-              [118.43, 29.08],
-              [118.44, 29.05],
-              [118.46, 29.05],
-              [118.6, 28.95],
-              [118.64, 28.94],
-              [119.07, 28.51],
-              [119.25, 28.41],
-              [119.36, 28.28],
-              [119.46, 28.19],
-              [119.54, 28.13],
-              [119.66, 28.03],
-              [119.78, 28],
-              [119.87, 27.94],
-              [120.03, 27.86],
-              [120.17, 27.79],
-              [120.23, 27.76],
-              [120.3, 27.72],
-              [120.42, 27.66],
-              [120.52, 27.64],
-              [120.58, 27.63],
-              [120.64, 27.63],
-              [120.77, 27.63],
-              [120.89, 27.61],
-              [120.97, 27.6],
-              [121.07, 27.59],
-              [121.15, 27.59],
-              [121.28, 27.59],
-              [121.38, 27.61],
-              [121.56, 27.73],
-              [121.73, 27.89],
-              [122.03, 28.2],
-              [122.3, 28.5],
-              [122.46, 28.72],
-              [122.5, 28.77],
-              [122.54, 28.82],
-              [122.56, 28.82],
-              [122.58, 28.85],
-              [122.6, 28.86],
-              [122.61, 28.91],
-              [122.71, 29.02],
-              [122.73, 29.08],
-              [122.93, 29.44],
-              [122.99, 29.54],
-              [123.03, 29.66],
-              [123.05, 29.73],
-              [123.16, 29.92],
-              [123.24, 30.02],
-              [123.28, 30.13],
-              [123.32, 30.29],
-              [123.36, 30.36],
-              [123.36, 30.55],
-              [123.36, 30.74],
-              [123.36, 31.05],
-              [123.36, 31.14],
-              [123.36, 31.26],
-              [123.38, 31.42],
-              [123.46, 31.74],
-              [123.48, 31.83],
-              [123.48, 31.95],
-              [123.46, 32.09],
-              [123.34, 32.25],
-              [123.22, 32.39],
-              [123.12, 32.46],
-              [123.07, 32.48],
-              [123.05, 32.49],
-              [122.97, 32.53],
-              [122.91, 32.59],
-              [122.83, 32.81],
-              [122.77, 32.87],
-              [122.71, 32.9],
-              [122.56, 32.97],
-              [122.38, 33.05],
-              [122.3, 33.12],
-              [122.26, 33.15],
-              [122.22, 33.21],
-              [122.22, 33.3],
-              [122.22, 33.39],
-              [122.18, 33.44],
-              [122.07, 33.56],
-              [121.99, 33.69],
-              [121.89, 33.78],
-              [121.69, 34.02],
-              [121.66, 34.05],
-              [121.64, 34.08]
-            ]
-          }]
-        });
-      }, 0);
-
-      function renderBrushed(params) {
-        var mainSeries = params.batch[0].selected[0];
-
-        var selectedItems = [];
-        var categoryData = [];
-        var barData = [];
-        var maxBar = 30;
-        var sum = 0;
-        var count = 0;
-
-        for (var i = 0; i < mainSeries.dataIndex.length; i++) {
-          var rawIndex = mainSeries.dataIndex[i];
-          var dataItem = convertedData[0][rawIndex];
-          var pmValue = dataItem.value[2];
-
-          sum += pmValue;
-          count++;
-
-          selectedItems.push(dataItem);
+      if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+      }
+      var categoryData = ["物流1", "建筑工程2", "旅游3", "公用事业", "电子", "石油石化", "房地产", "现代服务", "交通运输", "农业", "制造业", "矿业", "化工", "建材", "科研", "金融", "冶金", "能源", "TMT", "商贸"],
+        barData = ["3799", "2095", "9592", "4369", "14358", "1563", "76788", "13795", "27472", "5934", "21018", "4825", "7734", "10991", "3195", "3926", "4878", "16101", "10268", "12773"];
+      var geoData = [
+        { name: '北京', value: 39012.85 },
+        { name: '天津', value: 8302.38 },
+        { name: '河北', value: 17861.16 },
+        { name: '上海', value: 3532.65 },
+        { name: '江苏', value: 29000.57 },
+        { name: '浙江', value: 17449.61 },
+        { name: '福建', value: 14408.89 },
+        { name: '山东', value: 35350.57 },
+        { name: '广东', value: 18040.56 },
+        { name: '海南', value: 21110.31 },
+        { name: '山西', value: 3894.62 },
+        { name: '安徽', value: 15670.89 },
+        { name: '江西', value: 1965.54 },
+        { name: '河南', value: 22799.95 },
+        { name: '湖北', value: 18263.52 },
+        { name: '湖南', value: 14749.59 },
+        { name: '内蒙古', value: 8668.87 },
+        { name: '广西', value: 1053.35 },
+        { name: '重庆', value: 8347.74 },
+        { name: '四川', value: 18180.29 },
+        { name: '贵州', value: 6889.63 },
+        { name: '云南', value: 9021.46 },
+        { name: '西藏', value: 873.79 },
+        { name: '陕西', value: 11437.9 },
+        { name: '甘肃', value: 3443.28 },
+        { name: '青海', value: 1903.44 },
+        { name: '宁夏', value: 1849.16 },
+        { name: '新疆', value: 6339.3 },
+        { name: '辽宁', value: 3708.6 },
+        { name: '吉林', value: 7212.96 },
+        { name: '黑龙江', value: 3743.29 }
+      ];
+      myChart.on("click", function(params) {
+        console.log(params);
+        if (params.componentSubType == "bar") {
+          this.setOption({
+            series: {
+              name: 'Top5',
+              data: convertData(geoData.sort(function(a, b) {
+                return b.value - a.value;
+              }).slice(0, 5)),
+            }
+          })
+          return;
         }
-
-        selectedItems.sort(function(a, b) {
-          return a.value[2] - b.value[2];
-        });
-
-        for (var i = 0; i < Math.min(selectedItems.length, maxBar); i++) {
-          categoryData.push(selectedItems[i].name);
-          barData.push(selectedItems[i].value[2]);
-        }
-
+        barData = barData.sort();
         this.setOption({
           yAxis: {
             data: categoryData
           },
           xAxis: {
-            axisLabel: { show: !!count }
+            axisLabel: { show: !!0 }
           },
-          title: {
-            id: 'statistic',
-            text: count ? '平均: ' + (sum / count).toFixed(4) : ''
-          },
+          //            title: {
+          //                id: 'statistic',
+          //                text: count ? '平均: ' + (sum / count).toFixed(4) : ''
+          //            },
           series: {
             id: 'bar',
-            data: barData
+            data: barData.sort(function(a, b) {
+              return a - b;
+            })
           }
-        });
-      }
+        })
+      })
 
 
       /* myChart.setOption({
@@ -827,7 +590,7 @@ export default {
       let chinaDom = echarts.init(document.getElementsByClassName('fullView_heatingPower')[0]);
 
 
-      var hours = [ '1月', '2月', '3月', '4月', '5月', '6月',
+      var hours = ['1月', '2月', '3月', '4月', '5月', '6月',
         '7月', '8月', '9月', '10月', '11月',
         '12月'
       ];
@@ -1072,10 +835,11 @@ export default {
           name: '预算 vs 开销（Budget vs spending）',
           type: 'radar',
           // areaStyle: {normal: {}},
-          data: [/*{
-            value: [4300, 10000, 28000, 35000, 50000, 19000],
-            name: '预算产值（Allocated Budget）'
-          },*/
+          data: [
+            /*{
+                        value: [4300, 10000, 28000, 35000, 50000, 19000],
+                        name: '预算产值（Allocated Budget）'
+                      },*/
             {
               value: [5000, 14000, 28000, 31000, 42000, 21000],
               name: '产值（Actual Spending）'
